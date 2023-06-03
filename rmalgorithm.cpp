065476ced1edcb9161de4d57a07434bc75d4d037
feichay10 <alu0101364544@ull.edu.es>
2023-06-03 18:32:20 +0100

    minor changes

--- rmalgorithm.cpp
+++ rmalgorithm.cpp
@@ -10,14 +10,16 @@ void RMAlgorithm::calculatePriorities() {
 }
 
 bool RMAlgorithm::rateMonotonic(int numTasks, int timeTask, int period, int deadline) {
+    if (rmGarantyTest()) {
 
+    }
 }
 
 // U_0 -> worst_case, U -> utilization
 // Si U_0 ≤ U ≤ U_0(N) => Se puede planificar
 // Si U_0(N) ≤ U ≤ 1 => No garantizado
 // Si 1 ≤ U => Sobrecarga
-int RMAlgorithm::rmGarantyTest(int n, QString nameTask, int timeOfTask, int period, int deadline) {
+bool RMAlgorithm::rmGarantyTest(int n, QString nameTask, int timeOfTask, int period, int deadline) {
     worst_case = ((float)n * (pow(2.0, 1.0/(float)n) - 1.0));
     utilization = 0.0;
 
@@ -30,4 +32,3 @@ int RMAlgorithm::rmGarantyTest(int n, QString nameTask, int timeOfTask, int peri
         return 3; // Overload
     }
 }
-
